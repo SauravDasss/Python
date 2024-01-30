137ee570acfb2f76de8dd5c28ee630a17534179b
SauravDasss <158020369+SauravDasss@users.noreply.github.com>
2024-01-30 11:49:46 +0530

    Add files via upload

---
+++ task.py
@@ -0,0 +1,9 @@
+name = input("Enter your name: ")
+age = input("Enter your age: ")
+gender = input("Enter your gender (M/F): ")
+weight = input("Enter your weight: ")
+
+print("Your name is: ", name)
+print("Your age is: ",age,"Years")
+print("Your genderis: ",gender)
+print("Your weight is: ",weight,"kg")
